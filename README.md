916b0ad433303e05a5a51118e563347e851d724d
Keen <2003keen@gmail.com>
2026-04-13 10:51:19 +0700

    Implement: Add book button with dialog for input book information

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Library
+คำสั่งสำหรับการCommit
+Implement สร้าง
+Fix แก้บัค
+Improve refactor/เพิ่มประสิทธิภาพ
+Remove ลบส่วนที่ไม่ได้ใช้
+
+()สำหรับแยกประเภทให้ชัดเจน
+Example.
+Improve(UI): Responsive Layout for mobile
--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 const library = [];
+main();
 
+function cancleAdd() {
+    const cancleBtn = document.querySelector("#cancle-add");
+    const dialog = document.getElementById("dialog");
+    cancleBtn.addEventListener("click", (e) => {
+        dialog.close();
+    })
+}
 
-// ข้อมูลสำหรับทดลองการทำงาน 
-const book1 = new book("Marthin", "A1", "P1","P2");
-const book2 = new book("Luther King", "A2", "P2","P2");
-const book3 = new book("Luther Kingฤฤฤ", "A2", "P2","P2");
+function confirmAdd(){
+    const confirmBtn = document.querySelector("#confirm-add");
+    const dialog = document.getElementById("dialog");
+    confirmBtn.addEventListener("click", (e)=> {
+        e.preventDefault();
+        const title = document.querySelector("#book-title");
+        const author = document.querySelector("#book-author");
+        const pages = document.querySelector("#book-pages");
+        const price = document.querySelector("#book-price");
+        const addBook = new book(title.value, author.value, pages.value, price.value);
+        dialog.close();
+        addBookToLibrary(addBook);
+        displayBook();
+    })
+}
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
+function displayDialogAddBook() {
+    const dialog = document.getElementById("dialog");
+    const activeDialogBtn = document.getElementById("active-dialog");
+    activeDialogBtn.addEventListener("click", () => {
+        dialog.showModal();
+    })
+}
+
+function genID() {
+    return crypto.randomUUID();
+}
 
 function book(title, author, pages, price) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.price = price;
-    this.id = crypto.randomUUID();
+    this.id = genID();
 }
+
 
 function addBookToLibrary(book) {
     library.push(book);
-    displayBook();
+        displayBook();
 }
+
 
 // เอาหนังสือจากArrayมาแสดง
 function displayBook() {
@@ -39,4 +68,18 @@ function displayBook() {
         cell3.textContent = book.pages;
         cell4.textContent = book.id;
     });
+}
+
+function main() {
+    // ข้อมูลสำหรับทดลองการทำงาน 
+    const book1 = new book("Marthin", "A1", "P1","P2");
+    const book2 = new book("Luther King", "A2", "P2","P2");
+    const book3 = new book("Luther Kingฤฤฤ", "A2", "P2","P2");
+
+    addBookToLibrary(book1);
+    addBookToLibrary(book2);
+    addBookToLibrary(book3);
+    confirmAdd();
+    displayDialogAddBook();
+
 }
